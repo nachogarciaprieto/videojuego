f5ff3b905d3c2176866d52a527249c3b896681b2
José Ignacio García Prieto <nachogarciaprieto@gmail.com>
2019-02-27 00:01:14 +0100

    primer commit proyecto sin modificar

---
+++ videoJuego/src/net/instituto/desa/Game.java
@@ -0,0 +1,15 @@
+package net.instituto.desa;
+
+public class Game {
+	
+	
+	
+	
+	static final String ABAJO = "Abajo";
+	static final String ARRIBA = "Arriba";
+	static final String IZQUIERDA = "Izquierda";
+	static final String DERECHA = "Derecha";
+	
+	
+	Jugador j;
+}
---
+++ videoJuego/src/net/instituto/desa/Jugador.java
@@ -0,0 +1,40 @@
+package net.instituto.desa;
+
+public class Jugador {
+	int y, x;
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public void movimiento(String m) {
+		
+		
+		
+		if (m.equalsIgnoreCase(Game.DERECHA)) {
+			setX(getX() + 1);
+		}
+		if (m.equalsIgnoreCase(Game.IZQUIERDA)) {
+			setX(getX() - 1);
+		}
+		if (m.equalsIgnoreCase(Game.ARRIBA)) {
+			setY(getY() - 1);
+		}
+		if (m.equalsIgnoreCase(Game.ABAJO)) {
+			setY(getY() + 1);
+		}
+	}
+
+}
